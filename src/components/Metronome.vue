e94bd5e56e15a25f2166e78ed7909c8821f2baa0
RichardZhiLi <948945081@qq.com>
2021-03-30 14:14:36 +0100

    加入了taptempo功能，完成了节拍器的初代版本

--- src/components/Metronome.vue
+++ src/components/Metronome.vue
@@ -25,7 +25,7 @@
         <v-row class="flex-column mb-2">
             <v-col class="title text-center">{{tempoName}}</v-col>
             <v-col class="text-center">
-                <v-icon v-for="n in currentBeat" :key="n" >{{circleActiveArr[n-1]?'mdi-circle':'mdi-circle-outline'}}</v-icon>
+                <v-icon v-for="circle in circleObj" :key="circle.id" >{{circle.isActive?'mdi-circle':'mdi-circle-outline'}}</v-icon>
                 </v-col>
         </v-row>
 
@@ -65,7 +65,7 @@
           single-line
           color="black"
           item-color="black"
-          v-model="currentBeat"
+          v-model="beatNum"
         ></v-select>
       </v-col>
     </v-row>
@@ -88,9 +88,11 @@
     <v-container class="fill-height">
          <v-row>
           <v-col class="text-center">
-              <v-btn rounded elevation="0" width="50%"  color="info">
+              <v-btn rounded elevation="0" width="50%"  color="info" @click="testBPM">
                   tap tempo
               </v-btn>
+              <v-col v-show="tapBPM">Your current tap BPM is {{tapBPM}}</v-col>
+              <v-col></v-col>
           </v-col>
       </v-row>
     </v-container>
@@ -99,6 +101,7 @@
 </template>
 <script>
 import tempoObj from '@/assets/tempos.js'
+import taptempo from '@/assets/taptempo.js'
 let audioCtx
 let interval
 export default {
@@ -114,17 +117,12 @@ export default {
       tempoName: function (){
         return this.tempoObj.mapBpmToName(this.$data.bpmValue)
       },
-      circleActiveArr:function(){
-          const circleActiveArr = []
-          for(let i = 0; i< this.currentBeat;i++){
-            circleActiveArr[i] = false
-          }
-          return circleActiveArr
-      },
       lengthofBeatPerSeconds:function(){
           const bpm = this.bpmValue
           const beatsPerSecond = bpm/60
           const lengthofBeatPerSeconds = 1/beatsPerSecond
+          if(this.currentNote === "eighth") return lengthofBeatPerSeconds/2
+          if(this.currentNote === "sixteenth") return lengthofBeatPerSeconds/4
           return lengthofBeatPerSeconds
       }
   },
@@ -132,8 +130,24 @@ export default {
       isPlaying:false,
       bpmValue:0,
       tempoObj,
-      currentBeat:4,
+      taptempo,
+      tapBPM:0,
+      beatNum:4,
+      currentBeat:0,
       currentNote:"quarter",
+      circleObj:[{
+        id:0,
+        isActive:false
+      },{
+        id:1,
+        isActive:false
+      },{
+        id:2,
+        isActive:false
+      },{
+        id:3,
+        isActive:false
+      }],
       musicNotes:{
           quarter:{
               isActived:true //default setting
@@ -148,6 +162,19 @@ export default {
       ,
       beats:[1,2,3,4,5,6,7,8]
   }),
+  watch:{
+    beatNum:function(newValue){
+        const beatNum= newValue
+        const circleObj = []
+        for (let i = 0; i<beatNum;i++){
+            circleObj[i] = {
+              isActive:false,
+              id:i
+            }
+        }
+        return this.circleObj = circleObj
+      }
+  },
   methods:{
       play:function(){
         this.isPlaying = !this.isPlaying
@@ -156,20 +183,52 @@ export default {
           interval = undefined
           audioCtx.close()
           audioCtx = undefined
+          this.currentBeat = 0
+          for(let i =0;i<this.beatNum;i++) this.circleObj[i].isActive = false
           return 
           }
         if(!audioCtx) audioCtx = new AudioContext()
-        // const beat = this.currentBeat
-        // const note = this.currentNote
         this.playsound(this.lengthofBeatPerSeconds)
-        if(!interval) interval = setInterval(()=>this.playsound(this.lengthofBeatPerSeconds), this.lengthofBeatPerSeconds * 1000)
+        this.circleObj[this.currentBeat].isActive = true
+        if(!interval) interval = setInterval(()=>{
+          this.countBeat()
+          this.playsound(this.lengthofBeatPerSeconds)
+          }, this.lengthofBeatPerSeconds * 1000)
       },
       playsound:function (lengthofBeatPerSeconds){
         const oscillator = audioCtx.createOscillator();
-        oscillator.frequency.value = 800
-        oscillator.connect(audioCtx.destination)
+        const envelope = audioCtx.createGain();
+        oscillator.frequency.value = (this.currentBeat %this.beatNum == 0 )? 1000:800 //stress the first beat
+        envelope.gain.value = 1;
+        envelope.gain.exponentialRampToValueAtTime(1, audioCtx.currentTime + 0.001);
+        envelope.gain.exponentialRampToValueAtTime(0.001, audioCtx.currentTime + 0.02);
+        oscillator.connect(envelope)
+        envelope.connect(audioCtx.destination)
         oscillator.start()
         oscillator.stop(audioCtx.currentTime + lengthofBeatPerSeconds)
+      },
+      testBPM:function(){
+        this.taptempo.tap()
+        if(this.taptempo.timesCache.length >= 2) {
+          const bpm = Math.round(this.taptempo.calculateBPM())
+          return this.tapBPM = bpm 
+        }
+        return this.tapBPM = 0
+      },
+      countBeat:function(){
+        //this function is used to control the display of circle
+        /**
+         * Notice: The meaning of 'currentBeat' is used to tell user which beat he is currently in.
+         * On the other hand, this variable is corresponding to the index of circleObj, which is starting from 0 instead of 1.
+         * For example, in the user interface, the first circle will turn black if currentBeat values 0.
+         */
+
+        /**beatNum is what user chose from interface 'Beats' row */
+        if(this.currentBeat >= this.beatNum - 1) this.currentBeat = 0 //if currentBeat is approaching boundary, set it to the first beat, which is 0
+        else ++this.currentBeat //if currentBeat is not approaching boundary, then we directly + 1
+        if(this.currentBeat === 0) this.circleObj[this.beatNum-1].isActive = false //if currentBeat is 0, then its previous beat should be beatNum - 1. switch it off.
+        else this.circleObj[this.currentBeat-1].isActive = false //remember to switch off its previous beat 
+        this.circleObj[this.currentBeat].isActive = true
       },
       decrementBPMvalue:function(){
           this.$data.bpmValue --
